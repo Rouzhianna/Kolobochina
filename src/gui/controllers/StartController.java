1b630e260d4a3932249cd06038aa7f8f9e538415
Oleg Shatin 11-501 <mongoose0911@gmail.com>
2017-03-11 20:24:08 +0300

    refactored packages, added first layout, added change windows logic to StartController

--- src/gui/controllers/StartController.java
+++ src/gui/controllers/StartController.java
@@ -1,6 +1,12 @@
 package gui.controllers;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class StartController {
 
@@ -8,5 +14,17 @@ public class StartController {
 
     public void setStage(Stage stage) {
         this.stage = stage;
+    }
+
+    public void userClickedFirstButton(MouseEvent mouseEvent) throws IOException {
+        // TODO: 11.03.2017 add some logic to connect to serv
+        //this
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../choose_fighter_win.fxml"));
+        Parent root = loader.load();
+        ChoosingController controller = loader.getController();
+        controller.setStage(stage);
+        stage.setScene(new Scene(root, 800, 600));
+        stage.show();
+
     }
 }
